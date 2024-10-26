f39e5391edf9eeb89a50bb8829b18b174b9e4710
Sudeep Kaushal <sksalbert24@gmail.com>
2024-10-26 09:30:15 +0000

    added exit command support

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -17,6 +19,20 @@ func main() {
 
 		// Wait for user input
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", strings.TrimRight(command, "\n"))
+		programs := strings.Split(command, " ")
+
+		switch programs[0] {
+		case "exit":
+			if len(programs) > 2 {
+				errors.New("Wrong Arguments")
+			}
+			argument, error := strconv.Atoi(programs[1])
+			if error != nil {
+				errors.New("Wrong Argument")
+			}
+			os.Exit(argument)
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", strings.TrimRight(command, "\n"))
+		}
 	}
 }
